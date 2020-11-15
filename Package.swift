c919599bfaf0a15e400790d77a0b937593991cd7
Ricardo <rj.abreu89@gmail.com>
2020-11-15 02:42:37 +0000

    Fix manifest

--- Package.swift
+++ Package.swift
@@ -5,9 +5,8 @@ import PackageDescription
 
 let package = Package(
     name: "Libusb",
-    pkgConfig: "libusb",
-    providers: [
-        .brew(["libusb"]),
-        .apt(["libusb"])
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
     ]
 )
