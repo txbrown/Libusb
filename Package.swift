f333c01cae725839366260475aa8bf2da5ba6cfa
Ricardo <rj.abreu89@gmail.com>
2020-11-15 03:21:37 +0000

    Fix manifest

--- Package.swift
+++ Package.swift
@@ -12,11 +12,11 @@ let package = Package(
     products: [
         .library(name: "Libusb", targets: ["Libusb"]),
     ],
-        targets: [
-        .systemLibrary(name: "Libusb", path: "."),
-    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        .systemLibrary(name: "Libusb", path: "."),
     ]
 )
