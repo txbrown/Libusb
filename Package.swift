bd73916f6527a1fa2ef87264f881d7156af00ab7
Ricardo <rj.abreu89@gmail.com>
2020-11-15 03:13:41 +0000

    Fix manifest

--- Package.swift
+++ Package.swift
@@ -12,6 +12,5 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-    ],
-
+    ]
 )
