050e30546c564ead28df142b00bc0b638c8b51df
Ricardo <rj.abreu89@gmail.com>
2020-11-15 15:06:19 +0000

    Fix manifest

--- Package.swift
+++ Package.swift
@@ -9,6 +9,9 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(name: "libusb", targets: ["libusb"]),
     ],
+    dependencies: [
+         .package(path: "../Sources/libusb"),
+    ],
     targets: [
         .systemLibrary(name: "libusb", pkgConfig: "libusb-1.0"),
         .target(name: "Clibusb", dependencies: ["libusb"])
