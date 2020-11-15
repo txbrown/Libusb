29d5be23036538830cc2b32e762ddada13035f85
Ricardo <rj.abreu89@gmail.com>
2020-11-15 14:43:58 +0000

    Fix manifest

--- Package.swift
+++ Package.swift
@@ -4,9 +4,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "Libusb",
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+    name: "Clibusb",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(name: "Clibusb", targets: ["Clibusb"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .systemLibrary(name: "Clibusb", path: "./Sources/Modules", pkgConfig: "libusb-1.0", providers: [.brew(["libusb"])])
     ]
 )
